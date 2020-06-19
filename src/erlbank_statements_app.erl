66d3ad5d7c03e6113b9fecec71d780aac8bd84bf
Mike Sperber <sperber@deinprogramm.de>
2020-06-19 17:50:46 +0200

    Configuration issues for integration.

--- src/erlbank_statements_app.erl
+++ src/erlbank_statements_app.erl
@@ -16,12 +16,16 @@ start_cowboy() ->
 					{"/bank-statements/request",
 					 web_frontend, request}]}]),
     {ok, _} = cowboy:start_clear(my_http_listener,
-				 [{port, 8000}],
+				 [{port, 9001}],
 				 #{env => #{dispatch => Dispatch}}).
 
 start(_StartType, _StartArgs) ->
     % database:init_database(),
+    lager:info("Starting bank-statements service: ~p, cookie: ~p~n",
+	       [node(), erlang:get_cookie()]),
     start_cowboy(),
+    net_adm:ping('accounts@accounts-host'),
+    net_adm:ping('transactions@transactions-host'),
     erlbank_statements_sup:start_link().
 
 stop(_State) -> ok.
